aaab9abac33fb9898717ef8a6b268b08076f0f52
Vitaly Avseitsev <holitrixpecora@gmail.com>
2020-10-20 21:12:06 +0300

    change lang

--- Bump/Controllers/LangController.cs
+++ Bump/Controllers/LangController.cs
@@ -13,9 +13,8 @@ namespace Bump.Controllers
             Response.Cookies.Append(
                 CookieRequestCultureProvider.DefaultCookieName,
                 CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
-                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+                new CookieOptions {Expires = DateTimeOffset.UtcNow.AddYears(1)}
             );
-
             return LocalRedirect(returnUrl);
         }
     }
