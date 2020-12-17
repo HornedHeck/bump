e848d2498b2753d670ed9cb1643d94f6b52af4d8
Vitaly Avseitsev <vitaly.avseitsev@gmail.com>
2020-12-17 10:31:18 +0300

    Logs enabled

--- Bump/Program.cs
+++ Bump/Program.cs
@@ -1,26 +1,28 @@
 using System.Threading.Tasks;
+using Bump.Services.Log;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-namespace Bump {
-
-    public class Program {
-
-        public static async Task Main( string[] args ) {
-            var host = CreateHostBuilder( args ).Build();
+namespace Bump
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
             await host.RunAsync();
         }
 
-        public static IHostBuilder CreateHostBuilder( string[] args ) {
-            return Host.CreateDefaultBuilder( args )
-                // .ConfigureLogging(builder =>
-                // {
-                // builder.ClearProviders();
-                // builder.AddProvider(new LoggerProvider());
-                // })
-                .ConfigureWebHostDefaults( webBuilder => { webBuilder.UseStartup< Startup >(); } );
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(builder =>
+                {
+                    builder.ClearProviders();
+                    builder.AddProvider(new LoggerProvider());
+                })
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
         }
-
     }
-
 }
